4614b8d0289b79d98f0588fccf807fdcd069019d
Aleksandr Alekhin <aleksandera9@gmail.com>
2024-01-14 05:11:16 +0300

    feat: implement keyboard component

--- hangman/src/components/keyboard.js
+++ hangman/src/components/keyboard.js
@@ -7,13 +7,69 @@ export default class Keyboard extends Component {
     this.init();
   }
 
-  init() {}
+  init() {
+    this.container.onclick = (event) => {
+      if (event.target) {
+        const key = event.target.closest('[data-char]');
+        if (key) {
+          key.disabled = true;
+          /* eslint-disable-next-line no-underscore-dangle */
+          this._triggerEvent('onbutton', {key: key.dataset.char});
+        }
+      }
+    };
+  }
+
+  enabledAllButtons = () => {
+    const buttons = this.container.querySelectorAll('[data-char]');
+    buttons.forEach((btn) => {
+      const elem = btn;
+      elem.disabled = false;
+    });
+  };
 
   destroy() {}
 
   toHTML() {
-    return `
-      <div class="keyboard"></div>`;
+    const chars = [
+      'ё',
+      'й',
+      'ц',
+      'у',
+      'к',
+      'е',
+      'н',
+      'г',
+      'ш',
+      'щ',
+      'з',
+      'х',
+      'ъ',
+      'ф',
+      'ы',
+      'в',
+      'а',
+      'п',
+      'р',
+      'о',
+      'л',
+      'д',
+      'ж',
+      'э',
+      'я',
+      'ч',
+      'с',
+      'м',
+      'и',
+      'т',
+      'ь',
+      'б',
+      'ю',
+    ];
+
+    const keyElems = chars.map((char) => `<button class="keyboard__button" data-char="${char}">${char}</button>`);
+
+    return keyElems.join('');
   }
 
   render() {

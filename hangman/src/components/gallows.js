387c14eab0cc5fd9b7949a91386b06cfb8258ab9
Aleksandr Alekhin <aleksandera9@gmail.com>
2024-01-15 23:14:13 +0300

    fix: pictures path in gallows

--- hangman/src/components/gallows.js
+++ hangman/src/components/gallows.js
@@ -17,13 +17,13 @@ export default class Gallows extends Component {
 
   showMistakes = () => {
     const mistakesStyles = [
-      'url("../assets/gallows/gallows.png") no-repeat',
-      ',url("../assets/gallows/head.png") no-repeat 252px 149px',
-      ',url("../assets/gallows/torso.png") no-repeat 300px 250px',
-      ',url("../assets/gallows/hand-one.png") no-repeat 237px 270px',
-      ',url("../assets/gallows/hand-two.png") no-repeat 300px 270px',
-      ',url("../assets/gallows/leg-one.png") no-repeat 237px 378px',
-      ',url("../assets/gallows/leg-two.png") no-repeat 300px 378px',
+      'url("./assets/gallows/gallows.png") no-repeat',
+      ',url("./assets/gallows/head.png") no-repeat 252px 149px',
+      ',url("./assets/gallows/torso.png") no-repeat 300px 250px',
+      ',url("./assets/gallows/hand-one.png") no-repeat 237px 270px',
+      ',url("./assets/gallows/hand-two.png") no-repeat 300px 270px',
+      ',url("./assets/gallows/leg-one.png") no-repeat 237px 378px',
+      ',url("./assets/gallows/leg-two.png") no-repeat 300px 378px',
     ];
     const mistakes = this.store.getState().userData.numberOfMistakes;
     const picture = this.container.querySelector('.gallows-picture');

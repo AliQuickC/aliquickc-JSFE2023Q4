652939f4845ab3679f9ae72632808262c346b9b2
Aleksandr Alekhin <aleksandera9@gmail.com>
2024-03-12 10:36:09 +0300

    feat: RSS-PZ-06 implement greeting

--- rss-puzzle/src/components/start-page/start.ts
+++ rss-puzzle/src/components/start-page/start.ts
@@ -15,11 +15,17 @@ export default class StartPage extends BaseComponent {
   public destroy(): void {}
 
   private toHTML(): string {
+    const { firstName, lastName } = this.store.getState().userData;
     return `
     <div class="container start__container">
       <h2 class="start__title">RSS-Puzzle</h2>
       <p class="start__description">
         This is a fun game. Here you need to collect sentences from words. If you collect all the proposals, a painting by a famous artist will open.
+      </p>
+      <p class="start__greet">
+        Hello<br>
+        dear ${firstName} ${lastName}<br>
+        press the "Start" button to start the game
       </p>
       <button class="start__start-button">Start</button>
     </div>

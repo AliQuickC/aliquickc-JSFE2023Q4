308c557174a65aa69e7addc929f333953ae7bb07
Aleksandr <aleksandera9@gmail.com>
2024-04-03 13:09:08 +0300

    fix: input edit car view

--- async-race/src/components/garage-management/car-management.ts
+++ async-race/src/components/garage-management/car-management.ts
@@ -10,8 +10,8 @@ export default function carManagementLayout(createCarParam: CarParams, selectCar
       <button class="create-btn" id="create-btn" data-btn-name="create-btn">Create</button>
     </div>
     <div class="car-update">
-      <input type="text" name="" id="update-name" data-input-name="edit-name" autocomplete="off" ${selectCarParam !== null ? `value=${selectCarParam.name} ` : ' disabled'}>
-      <input type="color" name="" id="update-color" data-input-name="edit-color"  ${selectCarParam !== null ? `value=${selectCarParam.color} ` : ' disabled'}>
+      <input type="text" name="" id="update-name" data-input-name="edit-name" autocomplete="off" ${selectCarParam !== null ? `value="${selectCarParam.name}"` : ' disabled'}>
+      <input type="color" name="" id="update-color" data-input-name="edit-color"  ${selectCarParam !== null ? ` value="${selectCarParam.color}" ` : ' disabled'}>
       <button class="update-btn" id="update-btn" data-btn-name="update-btn" ${selectCarParam ? '' : 'disabled'}>Update</button>
     </div>
     <button class="generate-cars-btn" id="generate-cars" data-btn-name="generate-cars">Generate cars</button>
